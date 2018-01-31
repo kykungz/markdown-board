269490d63852ef1f55c5d5c6364128a27387ba45
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-01-31 18:37:05 +0700

    :lipstick: Change Loading indicator

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const CenteredReactLoading = styled(ReactLoading).attrs({
-  type: 'bars',
+  type: 'spin',
   delay: 0
 })`
   margin: auto;
